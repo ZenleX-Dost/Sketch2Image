5ac258c9a1d99d35e4cba06d400d216e65bba517
Your Name <your.email@example.com>
2024-12-06 17:41:31 +0100

    v17

--- conf.py
+++ conf.py
@@ -7,16 +7,17 @@ copyright = '2024, Amine EL Hend'
 author = 'Amine EL Hend'
 release = '6.12.24'
 master_doc = 'index'
-
+extensions = []
+"""
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon'
 ]
-
+"""
 templates_path = ['_templates']
 exclude_patterns = []
 
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = []
+html_static_path = ['_static']
